a2777ece875fcc10191e778469baddbc9899fa95
ofafa <chunghsianghsueh@gmail.com>
2015-12-25 17:50:16 +0800

    Initial commit: Basic scrape page and data manipulation with IMDB/archilife

---
+++ routes/scrape.js
@@ -0,0 +1,45 @@
+/**
+ * Created by s955281 on 12/25/15.
+ */
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var cheerio = require('cheerio');
+var request = require('request');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+    //the target url that we are going to scrape
+    //url = 'http://www.imdb.com/title/tt1229340/';
+    url = 'http://archilife.org';
+    request(url, function(err, res, html){
+        console.log('fetching data...');
+        if(!err){
+            console.log('analyzing data...');
+            var $ = cheerio.load(html);
+            var title, release, rating;
+            var json = { title:"", release:"", rating:""};
+            //start from an unique header class
+            $('.header_menu').filter(function(){
+                //see what is in the filtered content
+                console.log('filtering data...');
+                var data = $(this);
+                console.log(data);
+                /*
+                title = data.children().first().text();
+                release = data.children().last().children().text();
+                json.title = title;
+                json.release = release;
+                */
+            })
+        }
+        else{
+            console.log(err);
+        }
+    });
+    res.render('scrape', {title:"Hello World"});
+});
+
+
+
+module.exports = router;
